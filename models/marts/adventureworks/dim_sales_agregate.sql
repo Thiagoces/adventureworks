65a53981c0e08d7f29b023b6e2f680ec9558499e
thiagoces <thiagocesarfe@gmail.com>
2022-11-04 15:29:42 -0300

    finished model 1

--- models/marts/adventureworks/dim_sales_agregate.sql
+++ models/marts/adventureworks/dim_sales_agregate.sql
@@ -25,28 +25,60 @@ creditcard as (
     from {{ref ('stg_adventureworks_sales_creditcard')}}
 ),
 
-
-sales_agregate as (
+transform as (
     
     select 
-        sales_order_header_sales_reason.sales_order_id
+        order_header.sales_order_id
         , order_header.customer_id
         , order_header.territory_id
         , order_header.creditcard_id
+        , order_header.ship_to_address_id
         , order_header.order_date
         , order_header.status
         , sales_order_header_sales_reason.sales_reason_id
         , sales_reason.name as reason_name
         , creditcard.card_type
+        , (row_number () over (partition by order_header.sales_order_id order by order_header.order_date)) as one_reason
                 
     from order_header
-
     left join sales_order_header_sales_reason
     on sales_order_header_sales_reason.sales_order_id = order_header.sales_order_id
     left join sales_reason
     on sales_order_header_sales_reason.sales_reason_id = sales_reason.sales_reason_id
     left join creditcard
     on order_header.creditcard_id = creditcard.creditcard_id
+    
+),
+
+sales_agregate as (
+    select 
+        sales_order_id
+        , customer_id
+        , territory_id
+        , creditcard_id
+        , ship_to_address_id
+        , order_date
+        , status
+        , sales_reason_id
+        , reason_name
+        , card_type
+
+    from transform
+    where one_reason = 1
 )
 
 select * from sales_agregate
+
+-- with
+-- first_step as (
+--   select
+--     sh.salesorderid
+--    , sr.salesreasonid
+--    , sh.orderdate
+--    , (row_number () over (partition by sh.salesorderid order by sh.orderdate)) as d
+--   from `adventureworksdw.sales_salesorderheader` as sh
+--  left join adventureworksdw.sales_salesorderheadersalesreason as sr on sh.salesorderid = sr.salesorderid
+-- )
+
+-- select salesorderid, d, salesreasonid from first_step
+-- where d = 1
